6c185637d3a95c8573a5e2afac64d383638a3f52
SandroMaglione <lass.maglio@gmail.com>
2024-09-20 12:39:52 +0200

    use `JsonPlaceholderClient` in `Api`

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,10 +1,13 @@
-import { Effect, Layer } from "effect";
+import { Context, Effect, Layer } from "effect";
+import { JsonPlaceholderClient } from "./JsonPlaceholderClient";
 
-const make = {
+const make = Effect.map(JsonPlaceholderClient, (client) => ({
   getPosts: Effect.dieMessage("TODO"),
   getPostById: (id: string) => Effect.dieMessage("TODO"),
-};
+}));
 
-export class Api extends Effect.Tag("Api")<Api, typeof make>() {
-  static readonly Live = Layer.succeed(this, make);
+export class Api extends Context.Tag("Api")<Api, typeof make>() {
+  static readonly Live = Layer.succeed(this, make).pipe(
+    Layer.provide(JsonPlaceholderClient.Live)
+  );
 }
