461ec655ee0ec8aa8b12ed767fa2997e48d21664
SandroMaglione <lass.maglio@gmail.com>
2024-09-20 16:00:16 +0200

    execute effect in server component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
+import { Effect } from "effect";
+import { Api } from "../services/Api";
+import { RuntimeServer } from "../services/RuntimeServer";
+
 export const getConfig = async () => {
   return {
     render: "static",
   };
 };
 
+const main = Effect.gen(function* () {
+  const api = yield* Api;
+  return yield* api.getPosts;
+}).pipe(Effect.scoped);
+
 export default async function HomePage() {
+  const posts = await RuntimeServer.runPromise(main);
   return (
     <div>
       <title>Index</title>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
